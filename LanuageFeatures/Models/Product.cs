02eb5ae63914e8812d0bcb3d2ccbf6de948d4f67
DmitryDutov <dmitry_dutov@inbox.ru>
2021-09-22 12:58:06 +0300

    Добавьте файлы проекта.

---
+++ LanuageFeatures/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanuageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProduct()
+        {
+            Product kayak = new Product { Name = "Kayak", Price = 275M };
+            Product lifejacket = new Product { Name = "lifejacket", Price = 48.95M };
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
