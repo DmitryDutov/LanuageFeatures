571c7f39968dbf636b1001ed5be328e2e659b309
DmitryDutov <dmitry_dutov@inbox.ru>
2021-09-27 00:03:50 +0300

    Создание общего шаблона действия (лямбда)

--- LanuageFeatures/Controllers/HomeController.cs
+++ LanuageFeatures/Controllers/HomeController.cs
@@ -9,46 +9,12 @@ using System.Threading.Tasks;
 
 namespace LanuageFeatures.Controllers
 {
-    //Демонстрация рабты метода расширения как для класса Product так и для класса ShoppingCart
+    //Создание общего шаблона действия
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            //Инициализируем продукты в корзине
-            ShoppingCart cart = new ShoppingCart { Products = Product.GetProduct() };
-
-            //Инициализируем массив продуктов
-            Product[] productArray =
-            {
-                  new Product{Name="Kayak"      , Category="Cat001", Price = 500M }
-                , new Product{Name="LifeJacket" , Category="Cat002", Price = 300M }
-                , new Product{Name="Product-001", Category="Cat001", Price = 100M }
-                , new Product{Name="Product-002", Category="Cat001", Price = 150M }
-                , new Product{Name="Product-003", Category="Cat001", Price = 200M }
-            };
-
-            //вычисляем общую стоимость в обоих случаях
-            decimal cartTotal  = cart.TotalPrice();
-            decimal arrayTotal = productArray.TotalPrice();
-
-            //Общий прайс с фильтром
-            decimal priceFilterTotal = productArray.FilterByPrice(200).TotalPrice();
-            //Общий прайс с фильтром по имени
-            decimal nameFilterTotal  = productArray.FilterByName('P').TotalPrice();
-
-            //Универсальный фильтр с использованием лямбда-выражений
-            decimal uniPriceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 150).TotalPrice();
-            decimal uniNameFilterTotal  = productArray.Filter(p => p?.Name[0] == 'K').TotalPrice();
-
-            //выводим информацию на View
-            return View("Index", new string[] {
-                  $"Cart Total: {cartTotal:C2}" 
-                , $"Arry Total: {arrayTotal:C2}"
-                , $"Price Filter Total: {priceFilterTotal:C2}"
-                , $"Name Filter Total: {nameFilterTotal:C2}"
-                , $"UniPrice Filter Total: {uniPriceFilterTotal:C2}"
-                , $"UniName Filter Total: {uniNameFilterTotal:C2}"
-            });
+            return View(Product.GetProduct().Select(p => p?.Name));
         }
     }
 }
