a7663dd3a1a0fef8efce968ec0ead1316f9ce8ce
DmitryDutov <dmitry_dutov@inbox.ru>
2021-10-10 23:51:42 +0300

    Асинхронный метод с использованием ключевых слов async и await

--- LanuageFeatures/Models/MyAsyncMethods.cs
+++ LanuageFeatures/Models/MyAsyncMethods.cs
@@ -8,15 +8,13 @@ namespace LanuageFeatures.Models
 {
     public class MyAsyncMethods
     {
-        //Асинхронный метод без ключевых слов async и await
-        public static Task<long?> GetPageLenght()
+        //Асинхронный метод с использованием ключевых слов async и await
+        public async static Task<long?> GetPageLenght()
         {
             HttpClient client = new HttpClient();
-            var httpTask = client.GetAsync("http://apres.com");
+            var httpMessage = await client.GetAsync("http://apres.com");
 
-            return httpTask.ContinueWith((Task<HttpResponseMessage> antecednt)
-                =>
-            { return antecednt.Result.Content.Headers.ContentLength; });
+            return httpMessage.Content.Headers.ContentLength;
         }
     }
 }
