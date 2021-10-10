a373e02866d8e479b577c0cd26a0105e20d953bb
DmitryDutov <dmitry_dutov@inbox.ru>
2021-10-11 00:16:33 +0300

    Полученине имён: старый способ

--- LanuageFeatures/Controllers/HomeController.cs
+++ LanuageFeatures/Controllers/HomeController.cs
@@ -11,10 +11,19 @@ namespace LanuageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ViewResult> Index()
+        public ViewResult Index()
         {
-            long? lenght = await MyAsyncMethods.GetPageLenght();
-            return View(new string[] { $"Lenght:{ lenght }" });
+            var products = new[]
+            {
+                  new {Name = "Kaban", Price = 300M }
+                , new {Name = "Olen" , Price = 200M }
+                , new {Name = "Ejick", Price = 100M }
+                , new {Name = "Kotya", Price = 1000M }
+            };
+
+            //Старый подход к получению имён:
+            //Linq-выражение генерирует строки, каждая из которых имеет жёсткую ссылку на свойства Name и Price
+            return View(products.Select(p => $"Name: {p.Name}, Price: {p.Price}"));
         }
     }
 }
