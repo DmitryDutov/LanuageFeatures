d8ddde36d0640ebfe6614391c1e71835d1594f30
DmitryDutov <dmitry_dutov@inbox.ru>
2021-10-11 00:05:15 +0300

    HomeController - Асинхронный метод с использованием ключевых слов async и await

--- LanuageFeatures/Controllers/HomeController.cs
+++ LanuageFeatures/Controllers/HomeController.cs
@@ -11,18 +11,10 @@ namespace LanuageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
-            var products = new[] {
-                   //каждый объект в массиве products имеет анонимный тип
-                   new {Name = "Kayak"       , Price = 275M }
-                  ,new {Name = "Lifejacket"  , Price = 48.25M }
-                  ,new {Name = "Soccer ball" , Price = 19.50M }
-                  ,new {Name = "Conter flag" , Price = 34.95M }
-            };
-
-            //Отображение типа объекта по свойству
-            return View(products.Select(p => p.GetType().Name));
+            long? lenght = await MyAsyncMethods.GetPageLenght();
+            return View(new string[] { $"Lenght:{ lenght }" });
         }
     }
 }
